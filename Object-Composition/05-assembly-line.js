0f88c19c84003e47f7b031ace53502c922de7908
Tsvetelina Karamihova <ts.karamihova@gmail.com>
2022-10-12 14:30:43 +0300

    Exersice

---
+++ Object-Composition/05-assembly-line.js
@@ -0,0 +1,77 @@
+function createAssemblyLine(){
+
+    return {
+
+         hasClima(car){
+            car.temp = 21;
+            car.tempSettings = 21;
+            car.adjustTemp = function(){
+                if(car.temp < car.tempSettings){
+                    car.temp += 1;
+                } else if(car.temp > car.tempSettings){
+                    car.temp -= 1;
+                }
+            }
+        },
+    
+         hasAudio(car){
+            let track = {
+                name: null,
+                artist: null
+            }
+    
+            car.currentTrack = track;
+            car.nowPlaying = function(){
+                if(car.name !== null && car.artist !== null){
+                    console.log(`Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`)
+                }
+            }
+        },
+    
+         hasParktronic(car){
+            car.checkDistance = function(distance){
+    
+                let message = '';
+                if(distance < 0.1){
+                    message = 'Beep! Beep! Beep!';
+                } else if(distance >= 0.1 && distance < 0.25){
+                    message = 'Beep! Beep!';
+                } else if(distance >= 0.25 && distance < 0.5){
+                    message = 'Beep!';
+                }
+    
+                console.log(message);
+            }
+        }
+    }
+    
+
+}
+
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+
+console.log(myCar);
+
+
